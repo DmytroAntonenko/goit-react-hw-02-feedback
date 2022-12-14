eb397261db353bfeab05cab5eac764ab96d9a1b7
DmytroAntonenko <d.antonenko81@gmail.com>
2022-12-14 15:42:20 +0200

    1.5

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
 import React, {Component} from "react";
 
-import FeedbackOptions from "./FeedbackOptions/FeedbackOptions";
-import Section from "./Section/Section";
-import Statistics from "./Statistics/Statistics";
-import Notification from "./Notification/Notification";
+import FeedbackOptions from "./FeedbackOptions";
+import Section from "./Section";
+import Statistics from "./Statistics";
+import Notification from "./Notification";
 import css from './App.module.css';
 
 class App extends Component  {
